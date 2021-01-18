a707a038e69c048a70418387806659a7fe40e083
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-18 20:40:22 +0200

    q11 answer

---
+++ q11.js
@@ -0,0 +1,9 @@
+let num1=2154;
+let num2=458;
+let gcd=0;
+for(let counter=0; counter<=num1 && counter<=num2;counter++){
+    if(num1%counter===0 &&num2%counter===0){
+        gcd=counter;
+    }
+}
+console.log(gcd);
